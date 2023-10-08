0c8724e070239bfd0fe81447474d3852f978b9f7
sweta <sweta.koski@metropolia.fi>
2023-10-08 14:34:48 +0300

    edited

--- main.py
+++ main.py
@@ -14,47 +14,34 @@ def get_dict_airportcoordinate_by_country():
     dict_airport_coordinate={}
     sql="SELECT country,latitude_deg, longitude_deg from airport"
 
-    cursor=connection.cursor
+    cursor = connection.cursor()
     cursor.execute(sql)
-    result=cursor.fetchall()
+    result = cursor.fetchall()
     if cursor.rowcount>0:
       for row in result:
-          coordinate=(row[1], row[2])
-          dict_airport_coordinate[row[0]]=coordinate
+          coordinate = (row[1], row[2])
+          dict_airport_coordinate[row[0]] = coordinate
     return dict_airport_coordinate
 
-def calculate_distance(current_location, country_name):
+def calculate_distance(current_location, destination):
   my_db = connection.cursor()
   sql="SELECT country, latitude_deg, longitude_deg from airport WHERE country = %s"
   current_location=(current_location, )
-  country_name=(country_name, )
+  destination=(destination, )
   my_db.execute(sql, current_location)
   current_location_coordinate= my_db.fetchall()
   my_db.execute(sql, destination)
-  country_name_coordinate= my_db.fetchall()
+  destination_coordinate= my_db.fetchall()
   return calculate_distance.calculate_distance((current_location_coordinate['latitude_deg'], current_location_coordinate['longitude_deg']),
-          (country_name_coordinate['latitude_deg'], country_name_coordinate['longitude_deg'])).km
+          (destination_coordinate['latitude_deg'], destination_coordinate['longitude_deg'])).km
 
 
 def get_dict_distance(current_location):
     dict_distance={}
-    current_lat,current_lon=current_location
+    current_location_coordinate=current_location
     airport_coordinates=get_dict_airportcoordinate_by_country()
     for country,coordinates in airport_coordinates.items():
-        distance = calculate_distance()
+        distance = calculate_distance(current_location_coordinate, coordinates)
         dict_distance[country] = distance
     return dict_distance
 
-def command():
-  if command == 1:
-    country_name=input("Enter the name of the country you want to fly: ")
-    distance=calculate_distance(current_location, country_name)
-    need_fuel=fuel_consumed(distance)
-    if user_fuel>=need_fuel:
-        current_location=country_name
-        user_fuel -=need_fuel
-        if user_weapon>=need_weapon:
-            user_weapon=need_weapon+reward
-            user_money +=reward
-        else:
-          user_money=user_money/2
